bd2e5af80e58904fe1b7da5e50070344e7ce75ba
Luis Cervantes <luiscerv1229@gmail.com>
2020-07-02 13:18:17 -0700

    Added connections,routers,css,handlebars and created sql schema

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Burger', false);
+
--- server.js
+++ server.js
@@ -2,5 +2,27 @@ let express = require('express');
 
 let app = express();
 
-let port = process.env.PORT || 8080;
+let PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+let routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
